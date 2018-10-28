aab719a5c7b9330c64ca07179827c3c7608351d7
Adrien Vergnaud <a.vergnaud@gmail.com>
2018-10-28 18:02:19 +0100

    technos

--- impress.js/js/websocket_client.js
+++ impress.js/js/websocket_client.js
@@ -2,33 +2,39 @@
 ( function( document, window ) {
     "use strict";
    
-    if ("WebSocket" in window) {
-           document.querySelector("#imagination #console").insertAdjacentHTML( 'beforeend', "<p>WebSocket is supported by your Browser!</p>");
-           
-           // Let us open a web socket
-           var ws = new WebSocket("ws://localhost:8765");
-                       
-           ws.onopen = function() {
-             
-              // Web Socket is connected
-              document.querySelector("#imagination #console").insertAdjacentHTML( 'beforeend', "<p>Web Socket is connected</p>");
-           };
-                       
-           ws.onmessage = function (evt) {
-              var received_msg = evt.data;
-              document.querySelector("#imagination #messages").insertAdjacentHTML( 'afterbegin', "<p> - "+received_msg+"</p>");
-           };
-                       
-           ws.onclose = function() {
-             
-              // websocket is closed.
-              document.querySelector("#imagination #console").insertAdjacentHTML( 'beforeend', "<p>Connection is closed...</p>");
-           };
-        } else {
-         
-           // The browser doesn't support WebSocket
-              document.querySelector("#imagination #console").insertAdjacentHTML( 'beforeend', "<p>WebSocket NOT supported by your Browser!</p>");
-     }
 
+    function startWsClient() {
+
+        if ("WebSocket" in window) {
+               document.querySelector("#imagination #console").insertAdjacentHTML( 'beforeend', "<p>WebSocket is supported by your Browser!</p>");
+               
+               // Let us open a web socket
+               var ws = new WebSocket("ws://localhost:8765");
+                           
+               ws.onopen = function() {
+                 
+                  // Web Socket is connected
+                  document.querySelector("#imagination #console").insertAdjacentHTML( 'beforeend', "<p>Web Socket is connected</p>");
+               };
+                           
+               ws.onmessage = function (evt) {
+                  var received_msg = evt.data;
+                  document.querySelector("#imagination #messages").insertAdjacentHTML( 'afterbegin', "<p> - "+received_msg+"</p>");
+               };
+                           
+               ws.onclose = function() {
+                 
+                  // websocket is closed.
+                  document.querySelector("#imagination #console").insertAdjacentHTML( 'beforeend', "<p>Connection is closed...</p>");
+               };
+            } else {
+             
+               // The browser doesn't support WebSocket
+                  document.querySelector("#imagination #console").insertAdjacentHTML( 'beforeend', "<p>WebSocket NOT supported by your Browser!</p>");
+         }
+    }// fin startWsClient
+
+
+    document.querySelector('#imagination').addEventListener("click", startWsClient)
 
 } )( document, window );
