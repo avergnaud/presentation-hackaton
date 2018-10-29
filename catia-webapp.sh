27f6a6cc2679717435a5a0f46df58e98fcd531d0
Adrien Vergnaud <a.vergnaud@gmail.com>
2018-10-29 13:17:20 +0100

    fin

--- catia-webapp.sh
+++ catia-webapp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /home/ubuntu/dev4/_presentation-hackaton/orders_to_exchange
-node /home/ubuntu/dev3/pilotage_voix/orders_to_exchange/index.js
+cd /home/ubuntu/dev4/catia-webapp/
+ng serve --configuration=local
